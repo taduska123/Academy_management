14e12ddfb098b7a16e8837b4cd21656d12cb8b7c
taduska123 <taduska@gmail.com>
2021-01-12 19:58:35 +0200

    init

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Time;
 use App\Trainee;
 use Illuminate\Http\Request;
-use phpDocumentor\Reflection\Types\Boolean;
 
 class TimeController extends Controller
 {
@@ -16,55 +15,93 @@ class TimeController extends Controller
      */
     public function index($trainee_id)
     {
-        //return response()->json(Trainee::findorFail($id)->times()->get(), 200);
-        $months = Trainee::findorFail($trainee_id)
+        $times = Trainee::findorFail($trainee_id)
             ->times()
-            ->select(Time::raw("DATE_FORMAT(intership_day,'%m') as months") ,Time::raw("DATE_FORMAT(intership_day,'%d') as days"), 'intership_day', 'time_from', 'time_to', 'type_of_time')
+            ->select(
+                Time::raw("DATE_FORMAT(intership_day,'%m') as months"),
+                Time::raw("DATE_FORMAT(intership_day,'%Y') as years"),
+                Time::raw("DATE_FORMAT(intership_day,'%d') as days"),
+                'intership_day',
+                'time_from',
+                'time_to',
+                'type_of_time'
+            )
             ->orderBy('intership_day', 'asc')
             ->get();
-            foreach ($months as $month) {
-                if (null != $month->months) {
-                    $monthsarray[] = [
-                        'month' => $month->months, 'days' => [$this->days($months)]
-                    ];
-                }
-            }
-            $result = [
-                'months' => $monthsarray
-            ];
-        return response()->json($result, 200);
+
+        return response()->json($this->months($times), 200);
     }
 
     public function bymonth($trainee_id, Request $request)
     {
-        $days = Trainee::findorFail($trainee_id)
+        $times = Trainee::findorFail($trainee_id)
             ->times()
-            ->select(Time::raw("DATE_FORMAT(intership_day,'%d') as days"), 'intership_day', 'time_from', 'time_to', 'type_of_time')
+            ->select(
+                Time::raw("DATE_FORMAT(intership_day,'%m') as months"),
+                Time::raw("DATE_FORMAT(intership_day,'%Y') as years"),
+                Time::raw("DATE_FORMAT(intership_day,'%d') as days"),
+                'intership_day',
+                'time_from',
+                'time_to',
+                'type_of_time'
+            )
             ->whereYear('intership_day', $request->year)
             ->whereMonth('intership_day', $request->month)
             ->orderBy('intership_day', 'asc')
             ->get();
-        
-        return response()->json($this->days($days), 200);
+        //die($this->days($days));
+        //die($times);
+        return response()->json($this->days_of_month($times, $request->month, $request->year), 200);
     }
-    private function days($days)
+    private function months($times)
     {
-        foreach ($days as $day) {
-            if (null != $day->days) {
-                $daysarray[] = [
-                    'day' => $day->days, 'times' => [$this->times($days, $day)]
+        //die($times);
+        $monthfilter = null;
+        $yearfilter = null;
+        $montharray = [];
+        foreach ($times as $month) {
+
+            if ($monthfilter != $month->months ||  $yearfilter != $month->years) {
+                $montharray[] = [
+                    'year' => $month->years,
+                    'month' => $month->months,
+                    'days' => [$this->days_of_month($times, $month->months, $month->years)]
                 ];
+                $monthfilter = $month->months;
+                $yearfilter = $month->years;
             }
         }
-        $result = [
-            'days' => $daysarray
-        ];
-        return $result;
+        return $montharray;
     }
-    private function times($days, $day)
+    private function days_of_month($times, $month_check, $year_check)
     {
-        foreach ($days as $time) {
-            if ($time->days == $day->days) {
+
+        $dayarray = [];
+        $dayforfilter = null;
+        foreach ($times as $day) {
+            if (
+                $day->months == $month_check
+                && $day->years == $year_check
+                && $dayforfilter != $day->days
+            ) {
+
+                $dayarray[] = [
+                    'day' => $day->days, 'times' => [$this->times_of_day($times, $day->days, $month_check, $year_check)]
+                ];
+                $dayforfilter = $day->days;
+            }
+        }
+        return $dayarray;
+    }
+    private function times_of_day($times, $day_check, $month_check, $year_check)
+    {
+        $timesarray = [];
+        foreach ($times as $time) {
+            if (
+                $time->days == $day_check
+                && $time->months == $month_check
+                && $time->years == $year_check
+            ) {
                 $timesarray[] = [
                     'time_from' => $time->time_from,
                     'time_to' => $time->time_to,
@@ -98,7 +135,7 @@ class TimeController extends Controller
             ->whereDate('intership_day', $request->intership_day)
             ->get();
         if (!$this->check_if_times_overlap($times, $request->time_from, $request->time_to
-        || $times->isEmpty() )) {
+            || $times->isEmpty())) {
             $time = new Time;
             $time->trainee_id = $trainee_id;
             $time->intership_day = $request->intership_day;
@@ -117,20 +154,18 @@ class TimeController extends Controller
     {
         $from_compare = is_int($from_compare) ? $from_compare : strtotime($from_compare);
         $to_compare = is_int($to_compare) ? $to_compare : strtotime($to_compare);
-        foreach ($times as $time)
-        {
+        foreach ($times as $time) {
             // dd($time->time_from);
             $from = strtotime($time->time_from);
             $to = strtotime($time->time_to);
             //dd($from, $from_compare, $to, $to_compare);
             if (($from >= $from_compare && $from < $to_compare) ||
-            ($to > $from_compare && $to <= $to_compare)){
+                ($to > $from_compare && $to <= $to_compare)
+            ) {
                 return true;
             }
-            
         }
-         return false;
-            
+        return false;
     }
 
     public function set_contract_dates($trainee_id, Request $request)
--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware(['AuthKey'])->group(function () {
     Route::put('trainee/{trainee_id}', 'TraineeController@update');
     Route::delete('trainee/{trainee_id}', 'TraineeController@delete');
     Route::get('trainee/{trainee_id}/time', 'TimeController@index');
-    Route::get('trainee/{trainee_id}/timebyemonth', 'TimeController@bymonth');
+    Route::get('trainee/{trainee_id}/timebymonth', 'TimeController@bymonth');
     Route::post('trainee/{trainee_id}/time', 'TimeController@store');
     Route::put('trainee/{trainee_id}/time/{time_id}', 'TimeController@update');
     Route::put('trainee/{trainee_id}/contracts', 'TimeController@set_contract_dates');
